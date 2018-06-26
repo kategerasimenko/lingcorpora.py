2f0259eb551f502b984319547cbfa464a5b93e1e
kategerasimenko <katgerasimenko@gmail.com>
2018-06-26 17:18:06 +0400

    changed theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 AUTHOR = 'Lingcorpora team'
 SITENAME = 'lingcorpora.py'
 SITEURL = 'https://kategerasimenko.github.io/lingcorpora.py'
-THEME = 'theme/'
+THEME = 'pelican-sober/'
 
 PATH = 'content'
 
